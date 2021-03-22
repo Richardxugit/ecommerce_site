c685540437e417ffade3c23a6b91a49c16188e60
Richardxugit <cansolion@163.com>
2021-03-22 16:11:37 +1100

    fix store bug

--- src/services/store.js
+++ src/services/store.js
@@ -2,17 +2,16 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-let initialState =()=> {
-    initialState =
-        JSON.parse(window.localStorage.getItem('state')) || initialState;
+let initialState = () => {
+
     const middleware = [thunk];
 
     const store = createStore(
         rootReducer,
-        initialState,
+
         compose(
-            applyMiddleware(...middleware),
-            window.__REDUX_DEVTOOLS_EXTENSION__ &&window.__REDUX_DEVTOOLS_EXTENSION__()
+            applyMiddleware(...middleware)
+            // , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
         )
     );
 
